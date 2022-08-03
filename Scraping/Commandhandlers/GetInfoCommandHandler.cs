441c30d66fda86995255e73f42c5807903a21cea
Dan2ie0l <daniel.harutyunyan.20@gmail.com>
2022-08-03 20:42:50 +0400

    adding new commands

---
+++ Scraping/Commandhandlers/GetInfoCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Scraping.Comands;
+
+namespace Scraping.Commandhandlers
+{
+    public class DownloadCommandHandler : IRequestHandler<DownloadCommand, string[] >
+    {
+        
+        public async Task<string[]> Handle(DownloadCommand request, CancellationToken cancellationToken)
+        {
+            
+
+            return new string[] {};
+        }
+    }
+}
