34de5ef03a795116f6f80f2967284d17a291e9bb
Dan2ie0l <daniel.harutyunyan.20@gmail.com>
2022-08-10 18:02:46 +0400

    modifying request

--- Scraping/Commandhandlers/DownloadCommandHandler.cs
+++ Scraping/Commandhandlers/DownloadCommandHandler.cs
@@ -24,7 +24,7 @@ namespace Scraping.Commandhandlers
         public async Task<string[]> Handle(DownloadCommand request, CancellationToken cancellationToken)
         {
 
-            var url = "https://zappysys.com/blog/how-to-use-fiddler-to-analyze-http-web-requests/  ";
+            var url = "";
 
              var req = WebRequest.Create(url);
              req.Method = "GET";
@@ -41,13 +41,8 @@ namespace Scraping.Commandhandlers
              catch (WebException e) {
                  if (e.Message.Contains("302"))
                      {
-                     Console.WriteLine(e.Message+ " " + e.ToString);
-                     response = e.Response;
-                     using var webStream = response.GetResponseStream();
-                     using var reader = new StreamReader(webStream);
-                     var data = reader.ReadToEnd();
-                     Console.WriteLine(data);
-
+                     Console.WriteLine(e.Message);
+                     
                  }
 
              }
